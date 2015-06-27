5f96ce72ab10455c21d45524ab38670991fd3cf5
Dolan Murphy <admin@dmur.me>
2015-06-26 23:14:31 -0700

    Added Beer Pong game

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+	
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Called before making any physics calcs
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		if (!rb.useGravity) {
+			Vector3 translation = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+			gameObject.transform.Translate(translation * speed);
+		} else {
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			
+			rb.AddForce (movement * speed);
+		}
+
+
+		if (Input.GetMouseButtonDown (0) && !rb.useGravity) {
+			rb.useGravity = true;
+			rb.AddForce(0.0f, 0.0f, 0.5f);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
