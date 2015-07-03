766138d6ab9eec1d7a04d81e357fe748cb533e3f
Dolan Murphy <admin@dmur.me>
2015-07-03 16:38:13 -0700

    Added FPS debug text on wall and update every 10 frames

---
+++ Assets/Scripts/FramerateController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FramerateController : MonoBehaviour {
+
+	private int updateCount;
+	// Update is called once per frame
+	void Start () {
+		updateCount = 0;
+	}
+
+	void Update () {
+		if (updateCount % 10 == 0) {
+			TextMesh tm = gameObject.GetComponent ("TextMesh") as TextMesh;
+			tm.text = "Framerate\n" + (1 / Time.deltaTime);
+		}
+		updateCount++;
+	}
+}
